5d74d0aa2e146d26d12b02c9140d366df132e320
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-07-02 13:29:52 +0530

    setting up router

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,14 @@
-import { useRouterError} from "react-router-dom";
+import React from 'react'
+import { useRouteError} from "react-router-dom";
 
 const Error = () => {
-    const err = useRouterError();
+    const err = useRouteError();
+    console.log(err);
   return (
     <div>
       <h1>oooops! get away!</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Error;
