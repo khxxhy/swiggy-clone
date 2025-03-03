a6ae534c2bdfa3e5f24f769617a09f5cf60ecdd9
khxxhy <divyanshubawa9@gmail.com>
2025-03-03 21:46:37 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header.js"
-import Body from "./components/Body.js"
+import Header from "./components/Header.js";
+import Body from "./components/Body.js";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About.js"
+import About from "./components/About.js";
 import Contact from "./components/Contact.js";
 import Error from "./components/Error.js";
 
@@ -23,21 +23,21 @@ const AppRouter = createBrowserRouter([
     element: <App />,
     children: [
       {
-path: "/",
-element: <Body />
+        path: "/",
+        element: <Body />,
       },
       {
-      path: "/about",
-      element: <About />
-    },
-    {
-      path: "/contact",
-      element: <Contact />
-    },],
-    errorElement: <Error />
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
-
-])
+]);
 
 const AppWithRouter = () => {
   return <RouterProvider router={AppRouter} />;
